de27375ddfff6fe07fbb5b17ec53f4eb83dd52c7
David Högborg <d@hogborg.se>
2015-12-21 11:10:43 +0100

    chroot admin to home

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ if [ ! -e $archive ]; then
 	cd $archive
 	mkdir 1 2 3 4 5 6 7
 
-	pure-pw useradd admin -u ftpuser -D /home/ftpusers/ < /tmp/pw;
+	pure-pw useradd admin -u ftpuser -d /home/ftpusers/ < /tmp/pw;
 	pure-pw mkdb
 
 	echo "++++++++++++++++++++"
